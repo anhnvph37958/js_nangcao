5f66574fe2846d62a0c7ddb315f0855dffa06a77
anhnvph37958 <anhnvph37958@fpt.edu.vn>
2024-01-09 12:46:38 +0700

    update

---
+++ baitap_git/02_arrays/01_array_basic/script.js
@@ -0,0 +1,13 @@
+let x;
+
+const arr = [23,26,59,68,100];
+
+const mixed = [12,"hello",true,null];
+
+const fruit = new Array('apple','grape',"orange");
+
+
+arr1 = arr.slice(1,4);
+arr2 = arr.splice(1,4);
+
+console.log({arr1,arr2});
---
+++ baitap_git/02_arrays/02_array_metod/script.js
@@ -0,0 +1,27 @@
+let  x;
+
+// const arr = [29,38,47,100];
+
+// arr.push(100);
+
+// const obj1 = {a: 1,b: 3};
+// const obj2 = {c: 4, d:5};
+
+// const obj3 = {...obj1,...obj2};
+
+// console.log(obj3)
+// const newObj = {...person};
+
+const fruit = ['apple','lemon','banana'];
+const color = ['blue','red','brown'];
+
+x = fruit[2];
+
+fruits.push(color);
+x = arr.flat();
+console.log(x);
+x = fruit[1][2];
+
+const newObj = {...fruit,...color};
+
+console.log(newObj);
